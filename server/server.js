4fb8f2e43e9e1421db5c229a4092c4ea29df40f4
malikimran-kh <malikimranawan3124@gmail.com>
2026-04-17 08:28:53 +0500

    add cors origin setup

--- server/server.js
+++ server/server.js
@@ -8,9 +8,14 @@ const chatRoutes = require('./routes/chatRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(cors({
+  origin:"*"
+}))
 app.use('/api/chat', chatRoutes);
 
+app.use("/api" , (req,res)=>{
+  res.send("api is running")
+})
 mongoose.connect(process.env.MONGO_URI).then(() => {
   app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
 });
